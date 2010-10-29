d9760e24bd5c21835a8f88aca20d15627e5ab706
Vood <Vood@685b7520-408f-437a-b242-530ffa71052c>
2010-10-29 17:43:44 +0000

    git-svn-id: svn+ssh://osll.spb.ru/svn/geo2tag/branches/tracker_alpha@508 685b7520-408f-437a-b242-530ffa71052c

--- src/gui/inc/OnLineInformation.h
+++ src/gui/inc/OnLineInformation.h
@@ -130,7 +130,7 @@ namespace GUI
         void onSubscribeChannelQueryResponseReceived(QString status);
         void onUnsubscribeChannelQueryResponseReceived(QString status);
         void onSubscribedChannelsListQueryResponseReceived(CHandlePtr<common::Channels>& channels);
-        void onLoginQueryResponseReceived(QString status, QString auth_token,QString status description);
+        void onLoginQueryResponseReceived(QString status, QString auth_token);
 
     private:
         OnLineInformation(const OnLineInformation& obj);
