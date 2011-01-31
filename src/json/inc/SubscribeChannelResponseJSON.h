e217d24a88fa37c3ab66abf877147c5d8830b6c3
Vood <Vood@685b7520-408f-437a-b242-530ffa71052c>
2011-01-31 16:38:37 +0000

    classes for unsubscribe query
    git-svn-id: svn+ssh://osll.spb.ru/svn/geo2tag/trunk@544 685b7520-408f-437a-b242-530ffa71052c

---
+++ src/json/inc/SubscribeChannelResponseJSON.h
@@ -0,0 +1,8 @@
+#ifndef SUBSCRIBERESPONSEJSON_H
+#define SUBSCRIBERESPONSEJSON_H
+
+#include "DefaultResponseJSON.h"
+
+typedef DefaultResponseJSON SubscribeChannelResponseJSON;
+
+#endif // SUBSCRIBERESPONSEJSON_H
