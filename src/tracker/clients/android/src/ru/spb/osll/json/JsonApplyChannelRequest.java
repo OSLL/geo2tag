207b491c618dbd0e9a3680927a9d01de533a8571
bac1ca <bac1ca89@gmail.com>
2011-03-30 17:56:15 +0400

    code cleun up

--- src/tracker/clients/android/src/ru/spb/osll/json/JsonApplyChannelRequest.java
+++ src/tracker/clients/android/src/ru/spb/osll/json/JsonApplyChannelRequest.java
@@ -7,7 +7,7 @@ import java.net.URISyntaxException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JsonApplyChannelRequest implements IRequest.IApplyChannel {
+public class JsonApplyChannelRequest extends JsonRequest implements IRequest.IApplyChannel {
 
 	private String m_authToken;
 	private String m_name;
@@ -15,7 +15,8 @@ public class JsonApplyChannelRequest implements IRequest.IApplyChannel {
 	private String m_url;
 	private Integer m_activeRadius;
 	
-	public JsonApplyChannelRequest(String authToken, String name, String description, String url, Integer activeRadius){
+	public JsonApplyChannelRequest(String authToken, String name, 
+			String description, String url, Integer activeRadius){
 		m_authToken = authToken;
 		m_name = name;
 		m_description = description;
@@ -23,27 +24,15 @@ public class JsonApplyChannelRequest implements IRequest.IApplyChannel {
 		m_activeRadius = activeRadius;
 	}
 	
-	public JSONObject doRequest(){
-		JSONObject resultJSON = null; 
-		try {
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put(AUTH_TOKEN, m_authToken);
-			jsonObject.put(NAME, m_name);
-			jsonObject.put(DESCRIPTION, m_description);
-			jsonObject.put(URL, m_url);
-			jsonObject.put(ACTIVE_RADIUS, m_activeRadius);
-			
-			resultJSON = JsonBase.instance().doRequest(jsonObject, new URI(REQUEST));
-		} catch (JSONException e) {
-			e.printStackTrace();
-			// TODO Auto-generated catch block
-		} catch (IOException e) {
-			e.printStackTrace();
-			// TODO: handle exception
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			// TODO Auto-generated catch block
-		}
-		return resultJSON;
+	@Override
+	protected JSONObject doRequestInternal() throws JSONException, IOException,
+			URISyntaxException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put(AUTH_TOKEN, m_authToken);
+		jsonObject.put(NAME, m_name);
+		jsonObject.put(DESCRIPTION, m_description);
+		jsonObject.put(URL, m_url);
+		jsonObject.put(ACTIVE_RADIUS, m_activeRadius);
+		return JsonBase.instance().doRequest(jsonObject, new URI(REQUEST));
 	}	
 }
--- src/tracker/clients/android/src/ru/spb/osll/json/JsonLoginRequest.java
+++ src/tracker/clients/android/src/ru/spb/osll/json/JsonLoginRequest.java
@@ -7,8 +7,7 @@ import java.net.URISyntaxException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JsonLoginRequest implements IRequest.ILogin {
-	
+public class JsonLoginRequest extends JsonRequest implements IRequest.ILogin {
 	private String m_login;
 	private String m_password;
 	
@@ -16,22 +15,14 @@ public class JsonLoginRequest implements IRequest.ILogin {
 		m_login = login;
 		m_password = password;
 	}
-	
-	public JSONObject doRequest(){
-		JSONObject resultJSON = null; 
-		try {
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put(LOGIN, m_login);
-			jsonObject.put(PASSWORD, m_password);
-			resultJSON = JsonBase.instance().doRequest(jsonObject, new URI(REQUEST));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-		} catch (IOException e) {
-			// TODO: handle exception
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-		}
-		return resultJSON;
+
+	@Override
+	protected JSONObject doRequestInternal() throws JSONException, IOException,
+			URISyntaxException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put(LOGIN, m_login);
+		jsonObject.put(PASSWORD, m_password);
+		return JsonBase.instance().doRequest(jsonObject, new URI(REQUEST));
 	}
 
 }
