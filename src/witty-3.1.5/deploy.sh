c4fc022c1f039dcc4bd8964ab7eeabebfaf762da
Kirill Krinkin <kirill.krinkin@gmail.com>
2011-04-09 17:52:00 +0400

    removed old wt library sources

--- src/witty-3.1.5/deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Installing example ${APPBIN} to ${DEPLOYROOT}/${APPNAME}/ ..."
-mkdir -p ${DEPLOYROOT}/${APPNAME}
-install ${APPBIN} ${DEPLOYROOT}/${APPNAME}
-if [ "${APPRESOURCES}" ]; then
-  cp -R ${APPRESOURCES} ${DEPLOYROOT}/${APPNAME}
-fi
-echo "Done."
-
