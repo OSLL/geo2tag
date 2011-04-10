53275f3c594d435c0f459d75ebe9ad9745252b7c
bac1ca <bac1ca89@gmail.com>
2011-04-10 09:51:33 +0400

    ...

--- src/tracker/clients/android/src/ru/spb/osll/services/RequestService.java
+++ src/tracker/clients/android/src/ru/spb/osll/services/RequestService.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import ru.spb.osll.R;
 import ru.spb.osll.TrackerActivity;
 import ru.spb.osll.json.JsonAddUserRequest;
 import ru.spb.osll.json.JsonApplyChannelRequest;
@@ -22,23 +23,22 @@ import ru.spb.osll.preferences.Settings.ITrackerSettings;
 import ru.spb.osll.utils.TrackerUtil;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.IBinder;
 import android.util.Log;
 
 public class RequestService extends Service {
-	private static boolean TRAKER_STATUS = false;
-	public synchronized static boolean isActive(){
-		return TRAKER_STATUS;
-	}
-	public synchronized void setServiceStatus(boolean status){
-		TRAKER_STATUS = status;
-	}
 	
-	public static String SERVER;
-	
+	public static String SERVER; // FIXME bad smells in code
 	private String m_authToken;
-	
+
+	// get in settings
+	private String m_login;
+	private String m_password;
+	private String m_channel;
+	private String m_channelKey;
+	private String m_serverUrl;
 	
 	@Override
 	public void onCreate() {
@@ -50,8 +50,9 @@ public class RequestService extends Service {
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
 		Log.v(TrackerActivity.LOG, "request service start");
-
-		SERVER = new Settings(this).getPreferences().getString(ITrackerSettings.SERVER_URL, "");
+		getSettingsValues();
+		
+		SERVER = new Settings(this).getPreferences().getString(ITrackerSettings.SERVER_URL, ""); // TODO bad smells in code
 		setServiceStatus(true);
 		new Thread(loginRunnable).start();
 	}
@@ -68,6 +69,15 @@ public class RequestService extends Service {
 		return null;
 	}
 
+	private void getSettingsValues(){
+		SharedPreferences settings = new Settings(this).getPreferences();
+		m_login = settings.getString(ITrackerSettings.LOGIN, "");
+		m_password = settings.getString(ITrackerSettings.PASSWORD, "");
+		m_channel = settings.getString(ITrackerSettings.CHANNEL, "");
+		m_channelKey = settings.getString(ITrackerSettings.CHANNEL_KEY, "");
+		m_serverUrl = settings.getString(ITrackerSettings.SERVER_URL, "");
+	}
+	
 	private Runnable loginRunnable = new Runnable() {
 		@Override
 		public void run() {
@@ -77,13 +87,9 @@ public class RequestService extends Service {
 	
 	private final int ATTEMPT = 5;
 	private void login(){
-		Settings settings = new Settings(this);
-		String login = settings.getPreferences().getString(ITrackerSettings.LOGIN, "Makr");
-		String password = settings.getPreferences().getString(ITrackerSettings.PASSWORD, "test");
-		
 		JSONObject JSONResponse = null;
 		for(int i = 0; i < ATTEMPT; i++){
-			JSONResponse = new JsonLoginRequest(login, password).doRequest();
+			JSONResponse = new JsonLoginRequest(m_login, m_password).doRequest();
 			if (JSONResponse != null) 
 				break;
 		}
@@ -106,13 +112,9 @@ public class RequestService extends Service {
 	}
 	
 	private void addUser(){
-		Settings settings = new Settings(this);
-		String login = settings.getPreferences().getString(ITrackerSettings.LOGIN, "Makr");
-		String password = settings.getPreferences().getString(ITrackerSettings.PASSWORD, "test");
-		
 		JSONObject JSONResponse = null;
 		for(int i = 0; i < ATTEMPT; i++){
-			JSONResponse = new JsonAddUserRequest(login, password).doRequest();
+			JSONResponse = new JsonAddUserRequest(m_login, m_password).doRequest();
 			if (JSONResponse != null) 
 				break;
 		}
@@ -136,12 +138,10 @@ public class RequestService extends Service {
 	// TODO
 	private void applyChannel() {
 		Log.v(TrackerActivity.LOG, "applyChannel()");
-		
-		String channel = new Settings(this).getPreferences().getString(ITrackerSettings.CHANNEL, "Test channel");
-		
+				
 		JSONObject JSONResponse = null;
 		for (int i = 0; i < ATTEMPT; i++){
-			JSONResponse = new JsonApplyChannelRequest(m_authToken, channel,
+			JSONResponse = new JsonApplyChannelRequest(m_authToken, m_channel,
 					"Geo2Tag tracker channel", "http://osll.spb.ru/", 3000)
 					.doRequest();
 			if(JSONResponse != null)
@@ -157,7 +157,6 @@ public class RequestService extends Service {
 
 				new Thread(markRunnable).start();
 			} 
-			
 		} else {
 			// TODO apply channel fail
 		}
@@ -185,13 +184,11 @@ public class RequestService extends Service {
 //		if (location == null)
 //			return;
 //		Log.v(TrackerActivity.LOG, "location " + location);
-		
-		String channel = new Settings(this).getPreferences().getString(ITrackerSettings.CHANNEL, "Test channel");
-		
+				
 		JSONObject JSONResponse;
 		JSONResponse = new JsonApplyMarkRequest(
 				m_authToken,
-				channel,
+				m_channel,
 				"title",
 				"unknown",
 				"this tag was generated automaticaly by tracker application",
@@ -211,4 +208,13 @@ public class RequestService extends Service {
 		}
 	}
 	
+	
+	private static boolean TRAKER_STATUS = false;
+	public synchronized static boolean isActive(){
+		return TRAKER_STATUS;
+	}
+	public synchronized void setServiceStatus(boolean status){
+		TRAKER_STATUS = status;
+	}
+
 }
