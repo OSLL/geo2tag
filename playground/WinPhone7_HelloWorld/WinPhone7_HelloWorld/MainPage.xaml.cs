c6c1d4098cf9bcf88c1a9f07ec0e7618e0967d66
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2011-07-09 18:05:10 +0400

    Windows Phone HelloWorld added

---
+++ playground/WinPhone7_HelloWorld/WinPhone7_HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace WinPhone7_HelloWorld
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HelloWorld");
+        }
+    }
+}
