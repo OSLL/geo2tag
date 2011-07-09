93f584a1fcee7de9520a6044c9f7e0db1c149101
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2011-07-09 18:00:20 +0400

    Windows Mobile HelloWorld added

---
+++ playground/WinMobileHelloWorld/HelloWorld/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HelloWorld");
+        }
+    }
+}
