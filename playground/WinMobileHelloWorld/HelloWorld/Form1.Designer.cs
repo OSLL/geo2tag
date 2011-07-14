88170a16562e65d8cc92449b26b508a275d4f6f0
unknown <111@.(none)>
2011-07-14 13:58:49 +0400

    added Windows Mobile http example

--- playground/WinMobileHelloWorld/HelloWorld/Form1.Designer.cs
+++ playground/WinMobileHelloWorld/HelloWorld/Form1.Designer.cs
@@ -31,16 +31,44 @@
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 36);
+            this.button1.Location = new System.Drawing.Point(75, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 24);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "HelloWorld";
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 38);
+            this.label1.Text = "11";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "POST";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GET";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -48,6 +76,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
@@ -59,6 +90,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- playground/WinMobileHelloWorld/HelloWorld/Form1.cs
+++ playground/WinMobileHelloWorld/HelloWorld/Form1.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace HelloWorld
 {
@@ -19,6 +21,66 @@ namespace HelloWorld
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("HelloWorld");
+        }
+
+        // This method will do POST request
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+            label1.Text = "Sending POST request ...";
+            try
+            {
+                String postData = "{\"login\":\"Vasja\",\"password\":\"VasjaPWD\"}";
+                String url = "http://tracklife.ru/service?query=login";
+                label1.Text = "Creating HttpWebRequest";
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "POST";
+                req.KeepAlive = false;
+                req.AllowWriteStreamBuffering = true;
+                req.ContentType = "application/x-www-form-urlencoded";
+                
+                req.ContentLength = postData.Length;
+                req.Timeout = 20000;
+                label1.Text = "Getting request stream";
+                Stream requestStream = req.GetRequestStream();
+                label1.Text = "Writing POST data to request stream";
+                requestStream.Write(Encoding.ASCII.GetBytes(postData), 0,postData.Length);
+                requestStream.Close();
+
+                label1.Text = "closing request stream";
+                WebResponse resp = req.GetResponse();
+                Stream responseStream = resp.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                label1.Text = reader.ReadToEnd();
+                responseStream.Close();
+                reader.Close();
+            }
+            catch (WebException ex)
+            {
+               label1.Text += ex.Message;
+            }
+        }
+
+        // This method will do GET request
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Sending GET request ...";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://tracklife.ru");
+                req.Method = "GET";
+                WebResponse resp = req.GetResponse();
+                // Read response stream
+                Stream responseStream = resp.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                // Convert response stream to string
+                label1.Text = reader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                label1.Text = ex.Message;
+            }
         }
     }
 }
