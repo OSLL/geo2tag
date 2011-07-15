f8aedf3627a94a5b5628dcb54b29a3b64e75d98a
unknown <111@.(none)>
2011-07-15 14:33:00 +0400

    location example for Windows Mobile added(not working)

--- playground/WinMobileHelloWorld/HelloWorld/Form1.Designer.cs
+++ playground/WinMobileHelloWorld/HelloWorld/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -70,12 +71,22 @@
             this.button3.Text = "GET";
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 159);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 24);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Get location";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
@@ -93,6 +104,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- playground/WinMobileHelloWorld/HelloWorld/Form1.cs
+++ playground/WinMobileHelloWorld/HelloWorld/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,15 +7,63 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Collections;
 
 namespace HelloWorld
 {
     public partial class Form1 : Form
     {
+        [StructLayout(LayoutKind.Sequential)]
+        struct gps_info
+        {
+            internal int param1;
+            internal int param2;
+            internal int param3;
+            internal int param4;
+
+            internal short param21;
+            internal short param23;
+            internal short param22;
+            internal short param24;
+
+            internal short param25;
+            internal short param26;
+            internal short param27;
+            internal short param28;
+
+            internal double longitude;
+            internal double latitude;
+        }
+
+        IntPtr device = IntPtr.Zero;
+
+        IntPtr newLocationHandle = IntPtr.Zero;
+        IntPtr deviceStateChangedHandle = IntPtr.Zero;
+        IntPtr stopHandle = IntPtr.Zero;
+
         public Form1()
         {
             InitializeComponent();
+
+            newLocationHandle = CreateEvent(IntPtr.Zero, 0, 0, null);
+            deviceStateChangedHandle = CreateEvent(IntPtr.Zero, 0, 0, null);
+            stopHandle = CreateEvent(IntPtr.Zero, 0, 0, null);
+
+            device = GPSOpenDevice(newLocationHandle, deviceStateChangedHandle, null, 0);
+            if (device != IntPtr.Zero) label1.Text = "Device gotten";
+
         }
+
+
+
+
+        ~Form1()
+        {
+            GPSCloseDevice(device);
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -61,7 +108,6 @@ namespace HelloWorld
                label1.Text += ex.Message;
             }
         }
-
         // This method will do GET request
         private void button3_Click(object sender, EventArgs e)
         {
@@ -82,5 +128,75 @@ namespace HelloWorld
                 label1.Text = ex.Message;
             }
         }
+        // This will print current location
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Here will be code for location 
+
+            label1.Text = "Locating.....";
+
+            IntPtr position = Marshal.AllocHGlobal(1024*1024);
+            GPSGetPosition(device, position, 1000, 0);
+            
+            try
+            {
+                if (position != IntPtr.Zero)
+                {
+                    gps_info coordinates = (gps_info)Marshal.PtrToStructure(position, typeof(gps_info));
+                    label1.Text = coordinates.ToString();
+                    label1.Text = "Position: " + coordinates.latitude.ToString() + ","
+                        + coordinates.longitude.ToString();
+
+
+                    /*double[] coords = new double[2];
+                    
+                    Marshal.Copy(position, coords, sizeof(Int32) * (4+18) + sizeof(short) * 8 - 1, 2);
+                    label1.Text = coords[0].ToString() + "," + coords[1].ToString();*/
+
+                }
+                else 
+                {
+                    label1.Text = "Position is null!";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+            //
+            Marshal.FreeHGlobal(position);
+        }
+
+        #region PInvokes to gpsapi.dll
+        [DllImport("gpsapi.dll")]
+        static extern IntPtr GPSOpenDevice(IntPtr hNewLocationData, IntPtr hDeviceStateChange, string szDeviceName, int dwFlags);
+
+        [DllImport("gpsapi.dll")]
+        static extern int GPSCloseDevice(IntPtr hGPSDevice);
+
+        [DllImport("gpsapi.dll")]
+        static extern int GPSGetPosition(IntPtr hGPSDevice, IntPtr pGPSPosition, int dwMaximumAge, int dwFlags);
+
+        [DllImport("gpsapi.dll")]
+        static extern int GPSGetDeviceState(IntPtr pGPSDevice);
+        #endregion
+
+
+        #region PInvokes to coredll.dll
+        [DllImport("coredll.dll")]
+        static extern IntPtr CreateEvent(IntPtr lpEventAttributes, int bManualReset, int bInitialState, StringBuilder lpName);
+
+        [DllImport("coredll.dll")]
+        static extern int CloseHandle(IntPtr hObject);
+
+        const int waitFailed = -1;
+        [DllImport("coredll.dll")]
+        static extern int WaitForMultipleObjects(int nCount, IntPtr lpHandles, int fWaitAll, int dwMilliseconds);
+
+        const int eventSet = 3;
+        [DllImport("coredll.dll")]
+        static extern int EventModify(IntPtr hHandle, int dwFunc);
+
+        #endregion
     }
 }
