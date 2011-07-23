e9b0e0fb6205fce830a217a51e0a2281755e44fe
bac1ca <bac1ca89@gmail.com>
2011-07-23 22:53:18 +0400

    geo2tag web project sources

---
+++ src/webside/src/ru/spb/osll/web/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package ru.spb.osll.web.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+  void greetServer(String input, AsyncCallback<String> callback)
+      throws IllegalArgumentException;
+}
