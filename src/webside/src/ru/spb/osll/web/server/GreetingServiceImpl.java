d238eda85b01a3a7e6f248ef7626e7c5574b17aa
bac1ca <bac1ca89@gmail.com>
2011-07-30 15:22:03 +0400

    DBUtil, DB.properties && test request to database

--- src/webside/src/ru/spb/osll/web/server/GreetingServiceImpl.java
+++ src/webside/src/ru/spb/osll/web/server/GreetingServiceImpl.java
@@ -1,7 +1,14 @@
 package ru.spb.osll.web.server;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import ru.spb.osll.web.client.GreetingService;
+import ru.spb.osll.web.server.db.common.DBUtil;
 import ru.spb.osll.web.shared.FieldVerifier;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
@@ -12,36 +19,21 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
     GreetingService {
 
   public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid. 
     if (!FieldVerifier.isValidName(input)) {
-      // If the input is not valid, throw an IllegalArgumentException back to
-      // the client.
       throw new IllegalArgumentException(
           "Name must be at least 4 characters long");
     }
-
-    String serverInfo = getServletContext().getServerInfo();
-    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-    // Escape data from the client to avoid cross-site script vulnerabilities.
-    input = escapeHtml(input);
-    userAgent = escapeHtml(userAgent);
-
+    try{
+		Connection c = DBUtil.getConnction();
+		Statement Ex1Stmt = c.createStatement();
+		ResultSet result = Ex1Stmt.executeQuery("select login from users;");
+	    while (result.next()) {
+			input += "\n" + result.getString("login");
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
     return "Hello, " + input;
   }
 
-  /**
-   * Escape an html string. Escaping data received from the client helps to
-   * prevent cross-site script vulnerabilities.
-   * 
-   * @param html the html string to escape
-   * @return the escaped string
-   */
-  private String escapeHtml(String html) {
-    if (html == null) {
-      return null;
-    }
-    return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
-        ">", "&gt;");
-  }
 }
---
+++ src/webside/src/ru/spb/osll/web/server/db/common/DBUtil.java
@@ -0,0 +1,35 @@
+package ru.spb.osll.web.server.db.common;
+
+import java.sql.*;
+
+import org.scb.gwt.web.server.i18n.GWTI18N;
+
+public class DBUtil {
+
+	private static Connection connection = null;
+
+	public static Connection getConnction() {
+		if (connection == null) {
+			try {
+				DB dbconf = GWTI18N.create(DB.class);
+				final String host = dbconf.host();
+				final String port = dbconf.port();
+				final String base = dbconf.base();
+				final String user = dbconf.user();
+				final String pass = dbconf.pass();
+
+				final String preurl = "jdbc:postgresql://%s:%s/%s";
+				Class.forName("org.postgresql.Driver").newInstance();
+				System.setProperty("jdbc.drivers", "org.postgresql.Driver");
+				final String url = String.format(preurl, host, port, base);
+				connection = DriverManager.getConnection(url, user, pass);
+				return connection;
+			} catch (Exception e) {
+				// TODO use log4j
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+}
