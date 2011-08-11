559454c3311028a120957b73dbb76cad1ac5432d
bac1ca <bac1ca89@gmail.com>
2011-08-11 16:37:40 +0400

    Channels: code clean up

--- src/webside/src/ru/spb/osll/web/server/db/Channels.java
+++ src/webside/src/ru/spb/osll/web/server/db/Channels.java
@@ -1,6 +1,5 @@
 package ru.spb.osll.web.server.db;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
@@ -9,54 +8,63 @@ import org.apache.log4j.Logger;
 
 import ru.spb.osll.web.client.services.objects.Channel;
 import ru.spb.osll.web.client.services.objects.User;
-import ru.spb.osll.web.client.services.objects.Subscribe;
 import ru.spb.osll.web.server.db.common.DBUtil;
 
 public class Channels {
 	public static final String TABLE 		= "channel";
+	
 	public static final String ID 			= "id";
 	public static final String NAME 		= "name";
 	public static final String DESCRIPTION 	= "description";
 	public static final String URL 			= "url";
 
-	private static final String INSERT_CHANNEL = "INSERT INTO channel VALUES (%s, '%s', '%s', '%s');";
-	public static Channel insert(Channel channel){  
+	
+	
+	private static final String INSERT_CHANNEL 
+		= "INSERT INTO (name, description, url) channel VALUES ('%s', '%s', '%s');";
+	public static Channel insert(Channel ch){  
 		try {
-			final Connection c = DBUtil.getConnection();
-			final Statement Ex1Stmt = c.createStatement();
-			final ResultSet res = Ex1Stmt.executeQuery("SELECT MAX(id) FROM channel;");
-			long id = 0;
-			while (res.next()) {
-			id = res.getInt("max");
-			};
-			id = id + 1;
-			channel.setId(id);
-			final String query = String.format(INSERT_CHANNEL, id + "", channel.getName(), channel.getDescription(), channel.getUrl());
-			final int result = Ex1Stmt.executeUpdate(query);
+			final Statement statement = DBUtil.getConnection().createStatement();
+			final String query = String.format(INSERT_CHANNEL, ch.getName(), ch.getDescription(), ch.getUrl());
+			statement.execute(query, Statement.RETURN_GENERATED_KEYS); 
+			final ResultSet rs = statement.getGeneratedKeys();
+			rs.next();
+			final long id = rs.getLong(1); 
+			ch.setId(id);
+			return ch;
 		} catch (Exception e) {
 		    Logger.getLogger(Channels.class).error("insertChannel", e.getCause());
+		    return null;
 		}
-		return channel;
 	}
 
 	private static final String DELETE_CHANNEL = "DELETE FROM channel WHERE name='%s';";	
 	public static boolean delete(Channel channel){
-		boolean success = true; 
+		boolean success = false; 
 		try {
-			final Connection c = DBUtil.getConnection();
-			final Statement Ex1Stmt = c.createStatement();
+			final Statement statement = DBUtil.getConnection().createStatement();
 			final String query = String.format(DELETE_CHANNEL, channel.getName());
-			final int result = Ex1Stmt.executeUpdate(query);     
+			statement.execute(query);     
+			success = true;
 		} catch (Exception e) {
 			Logger.getLogger(Channels.class).error("deleteChannel", e.getCause());
-			success = false;
 		}
 		return success;
 	}	
 	
-	public static Channel update(Channel channel){
-		boolean success = delete(channel);
-		return insert(channel);
+	private static final String UPDATE_CHANNEL = 
+		"UPDATE channel SET name='%s', description='%s', url='%s' WHERE id='%s';";
+	public static boolean update(Channel ch){
+		boolean success = false;
+		try {
+			final Statement statement = DBUtil.getConnection().createStatement();
+			final String query = String.format(UPDATE_CHANNEL, ch.getName(), ch.getDescription(), ch.getUrl(), ch.getId());
+			statement.execute(query);
+			success = true;
+		} catch (Exception e) {
+			Logger.getLogger(Users.class).error("update", e.getCause());
+		}
+		return success;
 	}
 	
 	private static final String SELECT_ALL_CHANNELS = "SELECT * FROM channel;";
@@ -64,10 +72,9 @@ public class Channels {
 		Channel channel = null;
 		List<Channel> listChannels = new ArrayList<Channel>();
 		try {
-			final Connection c = DBUtil.getConnection();
-			final Statement Ex1Stmt = c.createStatement();
+			final Statement statement = DBUtil.getConnection().createStatement();
 			final String query = SELECT_ALL_CHANNELS;
-			final ResultSet result = Ex1Stmt.executeQuery(query);
+			final ResultSet result = statement.executeQuery(query);
 		    while (result.next()) {
 		    	channel = new Channel();	
 		    	channel.setId(result.getInt(ID)); 
@@ -82,32 +89,23 @@ public class Channels {
 	    return listChannels;
 	}
 
-	private static final String SELECT_BY_USER = "SELECT * FROM subscribes WHERE user_id='%s';";
-	private static final String SELECT_CHANNELS = "SELECT * FROM channel WHERE id='%s';";
-	public static final String CHANNEL_ID 		= "channel_id";
-	public static final String USER_ID 			= "user_id";
+
+	private static final String SELECT_BY_USER =
+			"SELECT * FROM channel INNER JOIN subscribe ON subscribe.channel_id = channel.id" +
+			" WHERE subscribe.user_id = %s;";
 	public static List<Channel> selectByUser(User user){
 		Channel channel = null;
-		Subscribe subscribe = null;
 		List<Channel> listChannels = new ArrayList<Channel>();
 		try {
-			final Connection c = DBUtil.getConnection();
-			final Statement Ex1Stmt = c.createStatement();
+			final Statement statement = DBUtil.getConnection().createStatement();
 			final String query = String.format(SELECT_BY_USER, user.getId());
-			final ResultSet result = Ex1Stmt.executeQuery(query);
-			long temp_channel_id;
-			String temp_query;
-			ResultSet temp_result;
+			final ResultSet result = statement.executeQuery(query);
 		    while (result.next()) {
-		    	subscribe = new Subscribe();	
-		    	temp_channel_id = result.getInt(CHANNEL_ID); 
-		    	temp_query = String.format(SELECT_CHANNELS, temp_channel_id);
-		    	temp_result = Ex1Stmt.executeQuery(temp_query);
 		    	channel = new Channel();
-		    	channel.setId(temp_result.getInt(ID)); 
-		    	channel.setName(temp_result.getString(NAME));
-		    	channel.setDescription(temp_result.getString(DESCRIPTION));
-		    	channel.setUrl(temp_result.getString(URL));
+		    	channel.setId(result.getLong(ID)); 
+		    	channel.setName(result.getString(NAME));
+		    	channel.setDescription(result.getString(DESCRIPTION));
+		    	channel.setUrl(result.getString(URL));
 		    	listChannels.add(channel);
 		    }
 		} catch (Exception e) {
@@ -116,34 +114,19 @@ public class Channels {
 	    return listChannels;
 	}
 
-	private static final String SELECT_UNUSER = "SELECT * FROM subscribe WHERE user_id!='%s';";
 	public static List<Channel> selectUnuse(User user){
-		Channel channel = null;
-		Subscribe subscribe = null;
-		List<Channel> listChannels = new ArrayList<Channel>();
-		try {
-			final Connection c = DBUtil.getConnection();
-			final Statement Ex1Stmt = c.createStatement();
-			final String query = String.format(SELECT_BY_USER, user.getId());
-			final ResultSet result = Ex1Stmt.executeQuery(query);
-			long temp_channel_id;
-			String temp_query;
-			ResultSet temp_result;
-		    while (result.next()) {
-		    	subscribe = new Subscribe();	
-		    	temp_channel_id = result.getInt(CHANNEL_ID); 
-		    	temp_query = String.format(SELECT_CHANNELS, temp_channel_id);
-		    	temp_result = Ex1Stmt.executeQuery(temp_query);
-		    	channel = new Channel();
-		    	channel.setId(temp_result.getInt(ID)); 
-		    	channel.setName(temp_result.getString(NAME));
-		    	channel.setDescription(temp_result.getString(DESCRIPTION));
-		    	channel.setUrl(temp_result.getString(URL));
-		    	listChannels.add(channel);
-		    }
-		} catch (Exception e) {
-			Logger.getLogger(Channels.class).error("selectChannelsByUser", e.getCause());
-		}
-	    return listChannels;
+		// TODO 
+		return null;
 	}	
+
+	public static boolean subscribeToChannel(Channel ch, User user){
+		// TODO
+		return true;
+	}	
+
+	public static boolean unsubscribeFromChannel(Channel ch, User user){
+		// TODO
+		return true;
+	}	
+
 }
--- src/webside/src/ru/spb/osll/web/server/db/Users.java
+++ src/webside/src/ru/spb/osll/web/server/db/Users.java
@@ -10,10 +10,12 @@ import ru.spb.osll.web.server.db.common.DBUtil;
 
 public class Users {
 	public static final String TABLE 	= "users";
+	
 	public static final String ID 		= "id";
 	public static final String LOGIN 	= "login";
 	public static final String PASSWORD	= "password";
 	public static final String TOKEN 	= "token";
+	
 	
 	private static final String INSERT_USER = "INSERT INTO users (login, password, token) VALUES ('%s', '%s', '%s');";
 	public static User insert(User user){
@@ -32,31 +34,34 @@ public class Users {
 		return null;
 	}
 
+	
 	private static final String DELETE_USER = "DELETE FROM users WHERE login='%s';";
 	public static boolean delete(User user){
+		boolean success = false;
 		try {
 			final Statement statement = DBUtil.getConnection().createStatement();
 			final String query = String.format(DELETE_USER, user.getLogin());
 			statement.execute(query);
-			return true;
+			success = true;
 		} catch (Exception e) {
 			Logger.getLogger(Users.class).error("delete", e.getCause());
-			return false;
 		}
+		return success;
 	}	
 
 	
 	private static final String UPDATE_USER = "UPDATE users SET password='%s', token='%s' WHERE login='%s';";
 	public static boolean update(User user){
+		boolean success = false;
 		try {
 			final Statement statement = DBUtil.getConnection().createStatement();
 			final String query = String.format(UPDATE_USER, user.getPassword(), user.getToken(), user.getLogin());
 			statement.execute(query);
-			return true;
+			success = true;
 		} catch (Exception e) {
 			Logger.getLogger(Users.class).error("update", e.getCause());
-			return false;
 		}
+		return success;
 	}
 
 	
