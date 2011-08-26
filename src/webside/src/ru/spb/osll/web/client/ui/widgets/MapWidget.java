a036e74d3c61bffc0be3c5d037c7d7f788b0b6f5
bac1ca <bac1ca89@gmail.com>
2011-08-26 21:45:25 +0400

    added MapWidget stub

---
+++ src/webside/src/ru/spb/osll/web/client/ui/widgets/MapWidget.java
@@ -0,0 +1,18 @@
+package ru.spb.osll.web.client.ui.widgets;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class MapWidget extends Composite {
+
+	public MapWidget() {
+		initWidget(onInitialize());
+	}
+	
+	private Widget onInitialize(){
+		// TODO
+		return null;
+	}
+	
+	
+}
