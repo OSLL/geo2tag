bf0ca18631e965cfe2e6bff1958c73cbc70b4275
Kirill Krinkin <kirill.krinkin@gmail.com>
2011-10-01 01:13:59 +0400

    btfy

--- src/common/src/GpsModeller.cpp
+++ src/common/src/GpsModeller.cpp
@@ -121,7 +121,7 @@ namespace common
         s << (const char *)lon << " " << (const char *) lat;
         setReady(true);
         s >> m_longitude >> m_latitude;
-	// This code doesnt used by service
+        // This code doesnt used by service
         //QThread::msleep(begin.msecsTo(currentModellerTime));
         QThread::msleep(begin.secsTo(currentModellerTime)*1000);
         begin=currentModellerTime;
