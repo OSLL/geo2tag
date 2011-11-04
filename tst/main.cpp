6b4cea8ac45e467df50f26965cc1eff3067c347e
krinkin <kirill.krinkin@gmail.com>
2011-11-04 10:34:22 +0400

    test commit from github

--- tst/main.cpp
+++ tst/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
   common::DataMark m;
   m.m_name = 'A';
   m.m_latitude = 60.1;
-  m.m_longitude = 30.;
+  m.m_longitude = 30.1;
   m.m_description = "Hello";
   marks.push_back(m);
   common::Picture p = maps::MapLoaderFactory::getInstance(maps::MapLoader::GOOGLE)->getMapWithMarks(60.,30.,12,800,480,marks);
