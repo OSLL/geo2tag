d58f52d2960ad1684f9d324f65d8c8582daa790d
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2012-02-02 11:02:25 +0000

    devel: test_platform fixed

--- automation/test_platform.sh
+++ automation/test_platform.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-response=`curl -d '{"login":"Mark","password":"test"}'  http://localhost:81/service?query=login`;
-correct_result='{ "auth_token" : "MMMMMMMMMM", "status" : "Ok", "status_description" : "Authorization was successful" }';
+response=`curl -d '{"login":"Mark","password":"test"}'  http://localhost:81/service/login`;
+#response=`curl -d '{"login":"Mark","password":"test"}'  http://localhost:81/service/login`;
+correct_result='{ "auth_token" : "MMMMMMMMMM", "errno" : 0 }';
 #if ! echo $response | grep -q -s -F Ok ; 
 if ! echo $response | grep -q -s -F "$correct_result"  ; 
 then
