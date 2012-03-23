082f0569e3dc4d5f7654a0538ab193949558bd54
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2012-03-23 09:08:42 +0000

    added debug output to test_platform

--- automation/test_platform.sh
+++ automation/test_platform.sh
@@ -5,6 +5,8 @@ correct_result='"errno" : 0';
 test_channel="test_$RANDOM";
 
 response_add_channel=`curl -d "{\"auth_token\":\"MMMMMMMMMM\", \"name\":\"$test_channel\", \"description\":\"\", \"url\":\"\", \"activeRadius\":30}"  http://localhost:81/service/addChannel`;
+echo "$response_add_channel"
+sleep 10s
 if ! echo $response_add_channel | grep -q -s -F "$correct_result"  ;
 then
 	echo "Fail at addChannel test"
