0d741db22ca6b02b81117f4fb95ed3f1c7b8dcc8
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2012-03-28 00:43:24 +0400

    #GT-560: modifed automation scripts

--- automation/clean_db.sh
+++ automation/clean_db.sh
@@ -25,7 +25,7 @@ su - postgres -c "dropuser geo2tag" >> ${dir_log}/build.log.txt 2>>${dir_log}/bu
 
 #DEPLOY
 dpkg -i ${dir_backup}/wikigps-libs_*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
-dpkg -i ${dir_backup}/wikigps-service_*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
+echo "n" | dpkg -i ${dir_backup}/wikigps-service_*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
 $CATALINA_HOME/bin/startup.sh >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
 # check, does db appears on machine
 db=`sudo -u postgres psql -A -q -t -c "select datname from pg_database" template1 | grep geo2tag`;
