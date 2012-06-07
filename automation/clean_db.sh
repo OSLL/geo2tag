096d503787314427cf6d8976d7d5184b38da301a
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2012-06-07 06:51:43 +0000

    fix for clean_db.sh

--- automation/clean_db.sh
+++ automation/clean_db.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
+#WEBGEO_HOME="/opt/geo2tag"
+#CATALINA_HOME="/opt/geo2tag/3rd_party/apache-tomcat-7.0.19"
 platform_repo="geo2tag-platform-repo"
 dir_automation="$WEBGEO_HOME/automation"
 dir_geo2tag="${dir_automation}/${platform_repo}"
 dir_log="${dir_automation}/platform_logs"
 dir_backup="${dir_automation}/platform_backup"
 
-AIROMO_FLAG="$1"
-if [ "$AIROMO_FLAG" == "AIROMO" ] ; then
-        platform_repo="airomo-platform-repo"
-        dir_geo2tag="${dir_automation}/${platform_repo}"
-fi
 
 #CREATE LOGS
 mkdir ${dir_log}
@@ -24,8 +20,8 @@ su - postgres -c "dropdb geo2tag" >> ${dir_log}/build.log.txt 2>>${dir_log}/buil
 su - postgres -c "dropuser geo2tag" >> ${dir_log}/build.log.txt 2>>${dir_log}/build.log.txt
 
 #DEPLOY
-dpkg -i ${dir_backup}/wikigps-libs_*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
-echo "n" | dpkg -i ${dir_backup}/wikigps-service_*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
+dpkg -i ${dir_backup}/libgeo2tag-*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
+echo "n" | dpkg -i ${dir_backup}/geo2tag-*  >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
 $CATALINA_HOME/bin/startup.sh >> ${dir_log}/deploy.log.txt 2>>${dir_log}/deploy.log.txt
 # check, does db appears on machine
 db=`sudo -u postgres psql -A -q -t -c "select datname from pg_database" template1 | grep geo2tag`;
@@ -35,6 +31,8 @@ then
 else
 	status="fail"
 fi
+echo $status
+#exit 0
 sleep 10s
 #TEST
 if ${dir_automation}/test_platform.sh >> ${dir_log}/test.log.txt 2>>${dir_log}/test.log.txt
@@ -52,7 +50,7 @@ curl -d '{"auth_token":"82dfa68f508feb7f6139327cdb837e69","channel":"default"}' 
 #SEND EMAIL
 cd ${dir_automation}
 test_summary=`cat ${dir_log}/test_summary.log `;
-ant -f mail_sender.xml -Dsubject "($status) geo2tag-platform night build ${AIROMO_FLAG}: build, test, deploy reports " -Dmessage "$test_summary" -Dlogdir "platform_logs"
+ant -f mail_sender.xml -Dsubject "($status) geo2tag-platform night build : build, test, deploy reports " -Dmessage "$test_summary" -Dlogdir "platform_logs"
 
 #CLEAN LOGS
 echo "" > ${dir_log}/build.log.txt
