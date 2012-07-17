bd3d5dd288a9bd4c029af90f5b482cc943e4911e
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-17 18:06:02 +0400

    GT-744 add steps

--- scripts/install_devenv.sh
+++ scripts/install_devenv.sh
@@ -2,10 +2,23 @@
 # Installation development environment for geo2tag
 # Note: tested on debian
 
+#TODO add checks for clean directory
 
 echo "Some commands will be executed with sudo, your local password maybe asked"
 
-echo "step 1: install building tools"
+echo "step: install building tools"
 sudo apt-get install build-essential libqt4-dev debhelper libxml2-dev libqjson-dev git
+
+echo "step: installation testing environment"
+
+echo "step: downloading sources"
+#uncomment next line if you have commit access on github.com
+##git clone git@github.com:OSLL/geo2tag.git
+git clone git://github.com/OSLL/geo2tag.git
+
+echo "step: build geo2tag platform part"
+cd geo2tag 
+./clean_build.sh
+
 
 
