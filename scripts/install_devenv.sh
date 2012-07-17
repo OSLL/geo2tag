0a064a076e254aba901703ba80532fc93bd712d2
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-17 21:29:40 +0400

    GT-744 r8 source address

--- scripts/install_devenv.sh
+++ scripts/install_devenv.sh
@@ -16,9 +16,9 @@ sudo echo "local all all trust" >>/etc/postgresql/9.1/main/pg_hba.conf
 echo "step: downloading sources"
 #uncomment next line if you have commit access on github.com
 ##git clone git@github.com:OSLL/geo2tag.git
-##git clone git://github.com/OSLL/geo2tag.git
+#git clone git://github.com/OSLL/geo2tag.git
 # testing
-git clone ssh://192.168.56.1/home/kkv/ws/p/geo2tag/
+#git clone ssh://192.168.56.1/home/kkv/ws/p/geo2tag/
 
 echo "step: build geo2tag platform part"
 cd geo2tag 
