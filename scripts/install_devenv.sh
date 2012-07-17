9850d3a4b8df38b14fb84ebe21b24a81ce66fa3c
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-17 21:42:29 +0400

    GT-744 r8 source address

--- scripts/install_devenv.sh
+++ scripts/install_devenv.sh
@@ -7,16 +7,19 @@
 echo "Some commands will be executed with sudo, your local password maybe asked"
 
 echo "step: install building tools"
-sudo apt-get install build-essential libqt4-dev debhelper libxml2-dev libqjson-dev libfcgi-dev git vim gitk libpq-dev
+sudo apt-get install --reinstall build-essential libqt4-dev debhelper libxml2-dev libqjson-dev libfcgi-dev git vim gitk libpq-dev postgresql postgresql-client
 
 echo "step: installation testing environment"
-sudo apt-get install postgresql postgresql-client
+sudo apt-get 
 sudo echo "local all all trust" >>/etc/postgresql/9.1/main/pg_hba.conf
 
 echo "step: downloading sources"
 #uncomment next line if you have commit access on github.com
 ##git clone git@github.com:OSLL/geo2tag.git
-#git clone git://github.com/OSLL/geo2tag.git
+
+# read only version
+git clone git://github.com/OSLL/geo2tag.git
+
 # testing
 #git clone ssh://192.168.56.1/home/kkv/ws/p/geo2tag/
 
