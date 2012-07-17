c285082855ec54c52418acbe3384037e34b4407c
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-17 18:17:13 +0400

    GT-744 more packages

--- scripts/install_devenv.sh
+++ scripts/install_devenv.sh
@@ -7,14 +7,16 @@
 echo "Some commands will be executed with sudo, your local password maybe asked"
 
 echo "step: install building tools"
-sudo apt-get install build-essential libqt4-dev debhelper libxml2-dev libqjson-dev git
+sudo apt-get install build-essential libqt4-dev debhelper libxml2-dev libqjson-dev git vim gitk
 
 echo "step: installation testing environment"
 
 echo "step: downloading sources"
 #uncomment next line if you have commit access on github.com
 ##git clone git@github.com:OSLL/geo2tag.git
-git clone git://github.com/OSLL/geo2tag.git
+##git clone git://github.com/OSLL/geo2tag.git
+# testing
+git clone ssh://192.168.56.1/home/kkv/ws/p/geo2tag/
 
 echo "step: build geo2tag platform part"
 cd geo2tag 
