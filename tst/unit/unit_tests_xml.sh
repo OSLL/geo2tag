e2060ce02d62cf987269ce93cf1423ecd202cf56
Kirill Krinkin <kirill.krinkin@gmail.com>
2012-07-28 13:42:26 +0400

    deleted extra logs

--- tst/unit/unit_tests_xml.sh
+++ tst/unit/unit_tests_xml.sh
@@ -7,17 +7,3 @@ for test in ${TESTS};
 do
 	${test} -xml 
 done;
-
-
-FAIL=`cat test.log | grep "FAIL" | wc -l`
-PASS=`cat test.log | grep "PASS" | wc -l`
-
-su - postgres -c "dropdb test_db"
-su - postgres -c "dropuser test_user"
-
-echo "" >>test_summary.log
-echo "See 'test.log' for results" >>test_summary.log
-echo "=================================================================">>test_summary.log
-echo "===  TESTING RESULTS Suites:${COUNT}   PASS/FAIL: ${PASS}/${FAIL}">>test_summary.log
-echo "=================================================================">>test_summary.log
-
